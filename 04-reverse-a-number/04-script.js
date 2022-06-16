b3db8ec893497e0a0d8f5396e1a909233ca3e6f1
julianmedwards <julian.michael.edwards@gmail.com>
2022-06-16 15:14:19 -0500

    Problem 4

--- 04-reverse-a-number/04-script.js
+++ 04-reverse-a-number/04-script.js
@@ -1,1 +1,22 @@
 'use strict'
+
+var input = 12345
+
+function reverseNumber(num) {
+    var numStr = num.toString()
+    var output = []
+
+    for (let i = numStr.length - 1; i > -1; i--) {
+        output.push(numStr[i])
+    }
+
+    output = output.join('')
+    return Number(output)
+}
+
+var answer = reverseNumber(input)
+
+console.log(answer)
+let answerEl = document.createElement('p')
+answerEl.innerHTML = '12345 reversed: ' + answer
+answers.append(answerEl)
