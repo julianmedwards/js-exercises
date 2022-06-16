25b8e71cf28e9dfdb713e77ed02395d48d3b3e53
julianmedwards <julian.michael.edwards@gmail.com>
2022-06-16 15:54:39 -0500

    Problem 5

--- 05-alphabetize-a-string/05-script.js
+++ 05-alphabetize-a-string/05-script.js
@@ -1,1 +1,15 @@
 'use strict'
+
+var input = 'Andrew'
+
+function alphabetize(string) {
+    let strArr = string.split('').sort().join('')
+    return strArr
+}
+
+let answer = alphabetize(input)
+
+console.log(answer)
+var answerEl = document.createElement('p')
+answerEl.innerHTML = 'Input of ' + input + ' alphabetized: ' + answer
+answers.append(answerEl)
