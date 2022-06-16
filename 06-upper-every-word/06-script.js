64e7d5aa6d4fb47196a653f35496122735cc106a
julianmedwards <julian.michael.edwards@gmail.com>
2022-06-16 16:55:33 -0500

    Problem 6

--- 06-upper-every-word/06-script.js
+++ 06-upper-every-word/06-script.js
@@ -1,1 +1,22 @@
 'use strict'
+
+var input = 'javascript is my favorite language'
+
+function upperAll(string) {
+    let strArr = string.split(' ')
+    strArr.forEach(function (word, index) {
+        let newWord = word.split('')
+        newWord[0] = newWord[0].toUpperCase()
+        strArr[index] = newWord.join('')
+    })
+
+    return strArr.join(' ')
+}
+
+let answer = upperAll(input)
+
+console.log(answer)
+
+let answerEl = document.createElement('p')
+answerEl.innerHTML = 'Uppercased sentence "' + input + '": ' + answer
+answers.append(answerEl)
