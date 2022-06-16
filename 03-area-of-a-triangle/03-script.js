5079f78b297dfb824a11081617051dffd8851519
julianmedwards <julian.michael.edwards@gmail.com>
2022-06-16 14:54:37 -0500

    Problem 3

--- 03-area-of-a-triangle/03-script.js
+++ 03-area-of-a-triangle/03-script.js
@@ -1,1 +1,28 @@
 'use strict'
+
+let myTriangle = {
+    sideA: 5,
+    sideB: 6,
+    sideC: 7,
+}
+
+// Find the area of a triangle using Heron's Formula.
+function heronsFormula(triangle) {
+    let a = triangle.sideA
+    let b = triangle.sideB
+    let c = triangle.sideC
+    let p = (a + b + c) / 2
+
+    let num = p * (p - a) * (p - b) * (p - c)
+    let area = Math.sqrt(num)
+    return area
+}
+
+var area = heronsFormula(myTriangle)
+
+let answerStr = 'Area of a Triangle with sides of 5, 6, and 7: ' + area
+
+console.log(answerStr)
+var answer = document.createElement('p')
+answer.innerHTML = answerStr
+answers.append(answerStr)
