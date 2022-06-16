af0d7ce5c7000808d23b1ea6d9daa67cb8cba5d6
julianmedwards <julian.michael.edwards@gmail.com>
2022-06-16 17:10:59 -0500

    Problem 7

--- 07-find-longest-word/07-script.js
+++ 07-find-longest-word/07-script.js
@@ -1,1 +1,22 @@
 'use strict'
+
+var input = 'A 24 week coding bootcamp is making me nerd out!'
+
+function findLongestWord(string) {
+    let strArr = string.split(' ')
+    let longest = 0
+    for (let i = 0; i < strArr.length; i++) {
+        if (strArr[i].length > strArr[longest].length) {
+            longest = i
+        }
+    }
+    return strArr[longest]
+}
+
+let answer = findLongestWord(input)
+
+console.log(answer)
+
+var answerEl = document.createElement('p')
+answerEl.innerHTML = 'Longest word in "' + input + '": ' + answer
+answers.append(answerEl)
